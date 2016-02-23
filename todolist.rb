0359eb012c5eeeac5e0ae6e92233a7ec66f10401
marlenaonrails <marlenabowen@gmail.com>
2016-02-22 21:19:52 -0500

    defined methods to match those called in udacitask.rb

--- todolist.rb
+++ todolist.rb
@@ -1,15 +1,66 @@
 class TodoList
+
 	attr_reader :title, :items 
+
    	def initialize(title)
    		@title = title
    		@items = Array.new
    	end
+
+   	def add_item(new_item)
+   		item = Item.new(new_item)
+   		@items.push(item)
+   	end
+
+   	def print_list
+   		puts
+   		puts "#{@author}'s #{@title}" 
+        puts "___________________________________"
+        print_items
+        puts "___________________________________"
+    end
+    
+    def print_items
+        @items.each do |item|
+            item.print_each_item(item)
+        end
+    end
+    
+    def delete_item(item_position)
+        @items.delete_at(item_position)
+    end
+        
+    def update_title
+        @title = "Task List"  
+    end
+    
+    def complete_item(item_position)
+        @items.at(item_position).complete_item_status
+    end
+
 end
 
 class Item
-    attr_reader :description, :completed_status 
+
+    attr_reader :description, :completed_status
+
     def initialize(item_description)
     	@description = item_description
     	@completed_status = false
     end
+
+    def completed_item_status
+    	@completed_status = true
+    end
+
+    def print_each_item(item)
+        puts
+        puts "Item: #{@description}"
+        puts "Due date: #{@due_date}"
+        puts "Completed status: #{@completed_status}" 
+        puts
+    end
+
+end
+
 end
